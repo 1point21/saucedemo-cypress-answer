ad024c1cab08db92a1b866ff7c5ca05c222d244d
1point21 <j.drinkall@mailfence.com>
2024-02-27 07:53:24 +0000

    feat: add commands to select and add login details

--- cypress/e2e/saucedemo-login.cy.js
+++ cypress/e2e/saucedemo-login.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+describe('test login page for saucedemo website', () => {
+  it('vists the saucedemo website', () => {
+    
+    //go to saucedemo webpage and check that url is correct
+    cy.visit('https://www.saucedemo.com/')
+    cy.url().should('include', 'saucedemo')
+
+    //select username field and type in username
+    cy.get('[data-test="username"]').type('standard_user')
+
+    // select password field and type in password
+    cy.get('[data-test="password"]').type('secret_sauce')
+
+    // click on "login" button
+    
   })
 })
